f3f4a83a1bd837f4e37098c049b3f75df464cce0
DmitrySPbRu <dmitryspbru@mail.ru>
2023-03-23 10:51:47 +0300

    Interface on Swing Main Window + combo box

--- Radar/EffScattArea.java
+++ Radar/EffScattArea.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import static java.lang.Math.*;
 public class EffScattArea {
     public static void main(String[] args) {
-        double sigma, dOb, lOb, wOb, teta, lambda, rOb, alfa;
+        double sigma, dOb, lOb, wOb, teta, lambda, rOb, alfa, sigmaTet;
         short typeOb;
         final double c=299792458; //m/sec
         Scanner in = new Scanner(System.in);
@@ -14,17 +14,20 @@ public class EffScattArea {
         lambda = in.nextDouble();
         System.out.print("Input foreshortening (deg): ");
         teta = in.nextDouble();
+        sigma = 0; sigmaTet = 0;
         switch (typeOb) {
             case 1: // ввод и расчет для пластины
             { System.out.print("Input length of object (m): ");
                 lOb = in.nextDouble();
                 System.out.print("Input width of object (m): ");
                 wOb = in.nextDouble();
-                sigma = (4*PI*pow(lOb,2)*pow(wOb,2))/pow(lambda,2); break; }
+                sigma = (4*PI*pow(lOb,2)*pow(wOb,2))/pow(lambda,2);
+                sigmaTet = sigma * cos(teta); break; }
             case 2: // ввод и расчет для диска
             { System.out.print("Input diameter of object (m): ");
                 dOb = in.nextDouble();
-                sigma = (4*pow(PI,3)*pow(dOb,4))/pow(lambda,2); break; }
+                sigma = (4*pow(PI,3)*pow(dOb,4))/pow(lambda,2);
+                sigmaTet = sigma * cos(teta); break; }
             case 3: // ввод и расчет для трехгранного уголка
             { System.out.print("Input length of edge (m): ");
                 lOb = in.nextDouble();
@@ -32,17 +35,20 @@ public class EffScattArea {
             case 4: // ввод и расчет для четырехгранного уголка
             { System.out.print("Input length of edge (m): ");
                 lOb = in.nextDouble();
-                sigma = (12*PI*pow(lOb,4))/pow(lambda,2); break; }
+                sigma = (12*PI*pow(lOb,4))/pow(lambda,2);
+                sigmaTet = sigma * cos(teta); break; }
             case 5: // ввод и расчет для сферы
             { System.out.print("Input diameter of object (m): ");
                 dOb = in.nextDouble(); rOb = dOb/2;
-                sigma = PI*pow(rOb,2); break; }
+                sigma = PI*pow(rOb,2);
+                sigmaTet = sigma * cos(teta); break; }
             case 6: // ввод и расчет для цилиндра
             { System.out.print("Input diameter of object (m): ");
                 dOb = in.nextDouble(); rOb = dOb/2;
                 System.out.print("Input length of object (m): ");
                 lOb = in.nextDouble();
-                sigma = (2*PI*rOb*pow(lOb,2))/lambda; break; }
+                sigma = (2*PI*rOb*pow(lOb,2))/lambda;
+                sigmaTet = sigma * cos(teta); break; }
             case 7: // ввод и расчет для конуса
             { System.out.print("Input diameter of object (m): ");
                 dOb = in.nextDouble(); rOb = dOb/2;
@@ -50,10 +56,13 @@ public class EffScattArea {
                 lOb = in.nextDouble();
                 if (lOb==0) { System.out.print("Input angle of cone (deg): ");
                 alfa = in.nextDouble(); } else alfa = 2*atan(rOb/lOb);
-                sigma = (PI*pow(rOb,2))*pow(tan(alfa),2); break; }
-            default: { System.out.println ("Type of object not selected"); sigma = 0; }
+                sigma = (PI*pow(rOb,2))*pow(tan(alfa),2);
+                sigmaTet = sigma * cos(teta); break; }
+            default: { System.out.println ("Type of object not selected");
+                 }
         }
        System.out.println ("Effective scattering area of object = " + sigma + " square meters");
-        in.close();
+       System.out.println ("ESA of object with foreshortening = " + sigmaTet + " square meters");
+       in.close();
     }
 }
