5951bb95243e224f75c9991c8b82abbf202623a5
DmitrySPbRu <dmitryspbru@mail.ru>
2023-10-26 17:31:09 +0300

    Change in AntGain

--- Radar/AntennaGain.java
+++ Radar/AntennaGain.java
@@ -3,31 +3,49 @@ import java.util.Scanner;
 import static java.lang.Math.*;
 public class AntennaGain {
     public static void main(String[] args) {
-        double dUp, kGain, eta, lambda, dAnt, dAntArr, rAnt;
+        double lAnt, hAnt, dUp, kGain, lambda, dAnt, wAnt, pitchArr, etaAnt;
         short typeAnt;
         final double c=299792458; //m/sec
         Scanner in = new Scanner(System.in);
-        System.out.println("Input type of antenna: 1 - phased array antenna, 2 - mirror antenna, 3 -  ");
+        System.out.println("Input type of antenna: 1 - phased array antenna, 2 - mirror antenna, 3 - horn ");
         typeAnt = in.nextShort();
         System.out.print("Input wavelength (m): ");
         lambda = in.nextDouble();
         switch (typeAnt) {
             case 1: // ввод и расчет для ФАР
-            {System.out.print("Input length of antenna (m): ");
-                dAnt = in.nextDouble();
+            {System.out.print("Введите ширину антенны (m): ");
+                wAnt = in.nextDouble();
+                System.out.print("Введите высоту антенны (m): ");
+                hAnt = in.nextDouble();
                 System.out.print("Input antenna array pitch (m): ");
-                dAntArr = in.nextDouble();
-                rAnt = dAnt / 2;
-                kGain = lambda / dAnt;
+                pitchArr = in.nextDouble();
+                System.out.print("Введите КПД антенны (эта): ");
+                etaAnt = in.nextDouble();
+                dAnt = wAnt * hAnt / pitchArr;
+                kGain = dAnt * etaAnt / lambda;
                 break;}
             case 2: // ввод и расчет для зеркальной
-            {System.out.print("Input length of antenna (m): ");
-                dAnt = in.nextDouble();
-                kGain = lambda / dAnt;
+            {System.out.print("Введите диаметр антенны (m): ");
+                lAnt = in.nextDouble();
+                System.out.print("Введите КПД антенны (эта): ");
+                etaAnt = in.nextDouble();
+                dAnt = lAnt * 2 * PI ;
+                kGain = dAnt * etaAnt / lambda;
+                break;}
+            case 3: // ввод и расчет для рупора
+            {System.out.print("Введите ширину антенны (m): ");
+                wAnt = in.nextDouble();
+                System.out.print("Введите высоту антенны (m): ");
+                hAnt = in.nextDouble();
+                System.out.print("Введите КПД антенны (эта): ");
+                etaAnt = in.nextDouble();
+                dAnt = wAnt * hAnt ;
+                kGain = dAnt * etaAnt / lambda;
                 break;}
             default: {System.out.println("Type of antenna not selected");
-                kGain = 0;}
+                kGain = 0; dAnt = 0; etaAnt = 0; }
         }
+        kGain = dAnt * etaAnt;
         System.out.println("Antenna gain = " + kGain);
         in.close();
     }
